e0a4027a98d968d830abba90a63196fe4bf207c3
ZeianRussell <150818055+ZeianRussell@users.noreply.github.com>
2024-09-09 21:26:38 +0800

    Update and rename source.lua to Movable.source.lua
    Movable Kavo UI Library

--- Movable.source.lua
+++ Movable.source.lua
@@ -2641,5 +2641,40 @@ function Kavo.CreateLib(kavName, themeList)
         return Sections
     end  
     return Tabs
+	local function makeDraggable(frame)
+    local dragging, dragInput, startPos
+
+    local function update(input)
+        local delta = input.Position - dragInput.Position
+        frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+    end
+
+    frame.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            dragging = true
+            dragInput = input
+            startPos = frame.Position
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    dragging = false
+                end
+            end)
+        end
+    end)
+
+    frame.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement then
+            dragInput = input
+        end
+    end)
+
+    game:GetService("UserInputService").InputChanged:Connect(function(input)
+        if dragging then
+            update(input)
+        end
+    end)
+	end
+
+	makeDraggable(frame)
 end
 return Kavo
